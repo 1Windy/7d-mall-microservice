e4893486d90caed8d07435694bf961e073fc0e89
zhenghf <1186787311@qq.com>
2024-05-22 21:03:05 +0800

    nested exception is java.lang.illegalstateexception: need to invoke method 'canceltimeoutorder' found on proxy for target class 'ordertimeoutcanceltask' but cannot be delegated to target bean. switch its visibility to package or protected.

--- mall-order/src/main/java/com/dunshan/mall/order/component/OrderTimeOutCancelTask.java
+++ mall-order/src/main/java/com/dunshan/mall/order/component/OrderTimeOutCancelTask.java
@@ -22,7 +22,7 @@ public class OrderTimeOutCancelTask {
      * 每10分钟扫描一次，扫描设定超时时间之前下的订单，如果没支付则取消该订单
      */
     @Scheduled(cron = "0 0/30 * ? * ?")
-    private void cancelTimeOutOrder(){
+    protected void cancelTimeOutOrder(){
         Integer count = portalOrderService.cancelTimeOutOrder();
         LOGGER.info("取消订单，并根据sku编号释放锁定库存，取消订单数量：{}",count);
     }
